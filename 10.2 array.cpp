7c2bbfd72aeb6ecb90574aaf7c4a2e2b9458437d
jei <jiraneeshafar9@gmail.com>
2025-12-27 15:03:09 +0700

    Add files via upload

---
+++ 10.2 array.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+	int A[12] = {12, 5, 7, 10, 5, 2, 17, 25, 9, 20, 35, 14};
+	int i;
+	printf("Isi Array A (1) :");
+	for(i=0; i<12; i++) {
+		printf("%4i", A[i]);
+	}
+	printf("\nIsi array A (2):");
+	for (i=11; i>=0; i--) {
+		printf("%4i", A[i]);
+	}
+	return 0;
+}
